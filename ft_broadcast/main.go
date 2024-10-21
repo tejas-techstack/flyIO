968a66f084f3f860ed3f258159c72674784b22c9
Tejas R <tejas.techstack@gmail.com>
2024-10-21 17:36:14 +0530

    challenges 3c, 3d

--- ft_broadcast/main.go
+++ ft_broadcast/main.go
@@ -1,78 +1,163 @@
-// TODO implementing Gossip protocol.
-
 package main
 
 import (
-  "encoding/json"
-  //"context"
-  "log"
-  "os"
-  //"sync"
+	"encoding/json"
+	"log"
+	"os"
+	"sync"
+	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
-type server struct{
-  n *maelstrom.Node;
-  nodeID string;
-  nodes []string;
+type server struct {
+	n            *maelstrom.Node
 
-  store []any;
+	store        []any
+	nodeTopology []any
+	seenMessages map[any]bool
+
+	toFlush      []any
+
+	mu           sync.Mutex
+	wg           sync.WaitGroup
 }
 
 type anymap map[string]any
 
-func makeServer() (* server){
-  var s * server
-  s.n = maelstrom.NewNode()
-  s.nodeID = s.n.ID()
-  s.nodes = s.n.NodeIDs()
+func main() {
+	s := server{
+		seenMessages: make(map[any]bool),
+	}
+	s.n = maelstrom.NewNode()
 
-  return s
+	s.n.Handle("topology", s.topology)
+	s.n.Handle("read", s.read)
+	s.n.Handle("broadcast", s.broadcast)
+	s.n.Handle("flush", s.flushHandler)
+
+	go s.startPeriodicFlush()
+
+	if err := s.n.Run(); err != nil {
+		log.Println("Run Error:", err)
+		os.Exit(1)
+	}
 }
 
-func main(){
-  s := makeServer();
-  log.Println(s)
+func (s *server) startPeriodicFlush() {
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
 
-  s.n.Handle("topology", s.topology);
-  s.n.Handle("read", s.read);
-  s.n.Handle("broadcast", s.broadcast);
-
-  if err := s.n.Run(); err != nil{
-    log.Println("Run Error: ", err)
-    os.Exit(1)
-  }
+	for range ticker.C {
+		log.Println("Triggering periodic flush")
+		err := s.flush()
+		if err != nil {
+			log.Println("Flush Error:", err)
+		}
+	}
 }
 
 func unmarshalJSON(msg maelstrom.Message) (map[string]any, error) {
-  var body anymap
+	var body anymap
 
-  if err := json.Unmarshal(msg.Body, &body); err != nil{
-    return nil, err
-  }
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+		return nil, err
+	}
 
-  return body, nil
+	return body, nil
 }
 
-func (s * server) topology(msg maelstrom.Message) error {
-  return s.n.Reply(msg, anymap{"type":"topology_ok",});
+func (s *server) topology(msg maelstrom.Message) error {
+	body, err := unmarshalJSON(msg)
+	if err != nil {
+		return err
+	}
+
+	s.mu.Lock()
+	s.nodeTopology = body["topology"].(map[string]any)[s.n.ID()].([]any)
+	s.mu.Unlock()
+
+	log.Printf("Topology for %s: %v", s.n.ID(), s.nodeTopology)
+
+	return s.n.Reply(msg, anymap{"type": "topology_ok"})
 }
 
-func (s * server) read(msg maelstrom.Message) error {
-  
-  body, err := unmarshalJSON(msg)
-  if err != nil {return err}
+func (s *server) read(msg maelstrom.Message) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 
-  s.store = append(s.store, body["message"])
-
-  return s.n.Reply(msg, anymap{"type": "read_ok","messages":s.store})
+	return s.n.Reply(msg, anymap{"type": "read_ok", "messages": s.store})
 }
 
-func (s * server) broadcast(msg maelstrom.Message) error {
-  
+func (s *server) broadcast(msg maelstrom.Message) error {
+	body, err := unmarshalJSON(msg)
+	if err != nil {
+		return err
+	}
 
+	message := body["message"]
+	s.mu.Lock()
+	if _, seen := s.seenMessages[message]; seen {
+		s.mu.Unlock()
+		return nil
+	}
+	s.seenMessages[message] = true
+	s.store = append(s.store, message)
+	s.mu.Unlock()
 
-  return s.n.Reply(msg, anymap{"type": "broadcast_ok",})
+	s.mu.Lock()
+	for _, node := range s.nodeTopology {
+		if node == s.n.ID() {
+			continue
+		}
+		if err := s.n.Send(node.(string), anymap{"type": "broadcast", "message": message, "fromNode": true}); err != nil {
+			log.Println(err)
+		}
+	}
+	s.mu.Unlock()
+
+	if body["fromNode"] != true {
+		return s.n.Reply(msg, anymap{"type": "broadcast_ok"})
+	}
+
+	return nil
+}
+
+func (s *server) flushHandler(msg maelstrom.Message) error {
+	log.Println("Handling Flush")
+	body, err := unmarshalJSON(msg)
+	if err != nil {
+		return err
+	}
+
+	for _, val := range body["message"].([]any) {
+		s.mu.Lock()
+		if _, seen := s.seenMessages[val]; seen {
+			s.mu.Unlock()
+			continue
+		}
+		s.seenMessages[val] = true
+		s.store = append(s.store, val)
+		s.mu.Unlock()
+	}
+
+	return nil
+}
+
+func (s *server) flush() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	log.Println("Beginning Flush")
+	for _, node := range s.n.NodeIDs() {
+		if node == s.n.ID() {
+			continue
+		}
+		if err := s.n.Send(node, anymap{"type": "flush", "message": s.store}); err != nil {
+			log.Println(err)
+			return err
+		}
+	}
+
+	return nil
 }
 
--- test/main.go
+++ test/main.go
@@ -1,88 +1,107 @@
-// TODO implementing Gossip protocol.
+// TODO: Implementing Gossip protocol.
 
 package main
 
 import (
-  "encoding/json"
-  //"context"
-  "log"
-  "os"
-  //"sync"
+	"encoding/json"
+	"log"
+	"os"
+	"sync"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
-type server struct{
-  n *maelstrom.Node;
-  store []any;
+type server struct {
+	n            *maelstrom.Node
+	store        []any
+	nodeTopology []any
+	seenMessages map[any]bool
+	mu           sync.Mutex // Mutex to protect shared resources
+	wg           sync.WaitGroup // WaitGroup to manage goroutines
 }
 
 type anymap map[string]any
 
-func main(){
-  s := server{};
-  s.n = maelstrom.NewNode()
+func main() {
+	s := server{
+		seenMessages: make(map[any]bool),
+	}
+	s.n = maelstrom.NewNode()
 
-  // log.Println(s)
+	s.n.Handle("topology", s.topology)
+	s.n.Handle("read", s.read)
+	s.n.Handle("broadcast", s.broadcast)
 
-  s.n.Handle("topology", s.topology);
-  s.n.Handle("read", s.read);
-  s.n.Handle("broadcast", s.broadcast);
-  s.n.Handle("unicast", s.unicast);
-
-  if err := s.n.Run(); err != nil{
-    log.Println("Run Error: ", err)
-    os.Exit(1)
-  }
+	if err := s.n.Run(); err != nil {
+		log.Println("Run Error:", err)
+		os.Exit(1)
+	}
 }
 
 func unmarshalJSON(msg maelstrom.Message) (map[string]any, error) {
-  var body anymap
+	var body anymap
 
-  if err := json.Unmarshal(msg.Body, &body); err != nil{
-    return nil, err
-  }
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+		return nil, err
+	}
 
-  return body, nil
+	return body, nil
 }
 
-func (s * server) topology(msg maelstrom.Message) error {
-  return s.n.Reply(msg, anymap{"type":"topology_ok",});
+func (s *server) topology(msg maelstrom.Message) error {
+	body, err := unmarshalJSON(msg)
+	if err != nil {
+		return err
+	}
+
+	s.mu.Lock() // Lock to protect shared resource
+	s.nodeTopology = body["topology"].(map[string]any)[s.n.ID()].([]any)
+	s.mu.Unlock()
+
+	log.Printf("Topology for %s: %v", s.n.ID(), s.nodeTopology)
+
+	return s.n.Reply(msg, anymap{"type": "topology_ok"})
 }
 
-func (s * server) read(msg maelstrom.Message) error {
-  return s.n.Reply(msg, anymap{"type": "read_ok","messages":s.store})
+func (s *server) read(msg maelstrom.Message) error {
+	s.mu.Lock() // Lock to protect shared resource
+	defer s.mu.Unlock()
+	return s.n.Reply(msg, anymap{"type": "read_ok", "messages": s.store})
 }
 
 func (s *server) broadcast(msg maelstrom.Message) error {
-  body, err := unmarshalJSON(msg)
-  if err != nil {
-    return err
-  }
+	body, err := unmarshalJSON(msg)
+	if err != nil {
+		return err
+	}
 
-  message := body["message"]
-  s.store = append(s.store, message)
+	message := body["message"]
+	s.mu.Lock() // Lock to protect shared resource
+	if _, seen := s.seenMessages[message]; seen {
+		s.mu.Unlock() // Unlock before returning
+		return nil // Ignore already seen messages
+	}
+	s.seenMessages[message] = true
+	s.store = append(s.store, message)
+	s.mu.Unlock() // Unlock after modifying shared resources
 
-  log.Println(s.n.NodeIDs())
-  for _, node := range s.n.NodeIDs() {
-    if node == s.n.ID() {
-      continue
-    }
+	for _, node := range s.nodeTopology {
+		if node == s.n.ID() {
+			continue
+		}
+		s.wg.Add(1) // Increment WaitGroup counter
+		go func(node any) {
+			defer s.wg.Done() // Decrement counter when goroutine completes
+			if err := s.n.Send(node.(string), anymap{"type": "broadcast", "message": message, "fromNode": true}); err != nil {
+				log.Printf("Failed to send message to %s: %v", node, err)
+			}
+		}(node)
+	}
 
-    // log.Printf("%v, %T\n", node, node)
-    s.n.Send(node, anymap{"type": "unicast", "message":body["message"],})
-  }
+	if body["fromNode"] != true {
+		return s.n.Reply(msg, anymap{"type": "broadcast_ok"})
+	}
 
-  return s.n.Reply(msg, anymap{"type": "broadcast_ok"})
-}
-
-func (s * server) unicast(msg maelstrom.Message) error {
-  body, err := unmarshalJSON(msg)
-  if err != nil {
-    return err
-  }
-  s.store = append(s.store, body["message"])
-
-  return nil
+	return nil
 }
 
